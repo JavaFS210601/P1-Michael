3748e10353edb51531ea4987a58cbae52b02ab66
unknown <michael.carson80@yahoo.com>
2021-07-18 23:17:15 -0400

    Front end structure

--- src/main/java/com/revature/daos/ReimbursementDAO.java
+++ src/main/java/com/revature/daos/ReimbursementDAO.java
@@ -13,17 +13,15 @@ public class ReimbursementDAO implements ReimbursementDAOInterface{
 
 	@Override
 	public void addReimbursement(Reimbursement reimbursement) {
-		//Open a hibernate Session object so that we can connect to the database
-				Session ses = HibernateUtil.getSession(); //there is some parallels between ConnectionUtil.getConnection() in JDBC!
+		
+				Session ses = HibernateUtil.getSession(); 
 				
-				ses.save(reimbursement); //use save method to insert into the data base it's a method for Insert fucntionality
+				ses.save(reimbursement); 
 				
-				HibernateUtil.closeSession(); //Close our Session (not super necessary here, but good practice)
+				HibernateUtil.closeSession(); 
 		
 	}
-//	*NEED TO FIX THIS ONE*
-	//using the list of reimbursement then getters to get in id and if it has the id for pending return using a for loop
-	
+
 	@Override
 	public List<Reimbursement> selectReimByStatus() {
 		Session ses = HibernateUtil.getSession();
@@ -45,7 +43,7 @@ public class ReimbursementDAO implements ReimbursementDAOInterface{
 		HibernateUtil.closeSession();
 		
 		}
-		return returnList; //remember, return ends the method, so we close the Session, then return
+		return returnList; 
 
 	}
 	
@@ -63,23 +61,19 @@ public class ReimbursementDAO implements ReimbursementDAOInterface{
 		return true;
         
 	}
-	//probably should be in the dao of the class this is actually in models this is actually in the user class so probably should be in the user dao
-	//parameter needs to be empty because the user doesnt need to give anything for this method in order for it
-	//to complete what it needs to do because what is going on is only in this block scope
-	//it can be here
+	
 	@Override
 	public List<Reimbursement> getReimbursements() {
 		Session ses = HibernateUtil.getSession();
 		
-		//Using HQL! Hibernate Query Language it references the Java class, not the DB table
 		
 		
-		List<Reimbursement> allList = ses.createQuery("FROM Reimbursement").list(); //at the end, we turn the Query object into a List
+		
+		List<Reimbursement> allList = ses.createQuery("FROM Reimbursement").list(); 
 		
 		
 		HibernateUtil.closeSession();
-		//for (Reimbursement r : allList) {
-            //not needed in this case to print only  the loop  all reimbursements if you are only getting one item or select items from the list
+		
         
 		return allList;
 	}
